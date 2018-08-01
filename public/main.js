db39791e5cc21650a886714ec2d7f152a85ad6c2
Kemenyfy <gustavo.kemenyfy@gmail.com>
2018-08-01 13:11:31 -0400

    Dice explorer mode

--- public/main.js
+++ public/main.js
@@ -1,5 +1,15 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
+const buttonClickEvent = () => {
+  const randomPick = generateRandomNumber()
+  console.log('clicked current number is ', randomPick)
+  const newp = document.createElement('p')
+    newp.textContent = randomPick
+    const parent = document.querySelector('.result')
+    parent.textContent = ''
+    parent.appendChild(newp)
 }
 
-document.addEventListener('DOMContentLoaded', main)
+const generateRandomNumber = () => {
+  return Math.ceil(Math.random() * 6)
+}
+
+document.querySelector('.click-here').addEventListener('click', buttonClickEvent)
